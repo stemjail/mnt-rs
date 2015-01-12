d16ad6ad0efa789a453e547cc893de6e6d9b290c
Mickaël Salaün <mickael.salaun@ssi.gouv.fr>
2015-01-12 18:59:46 +0100

    mnt: Rust update - allow unstable/experimental

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+#![allow(staged_unstable)]
+#![allow(staged_experimental)]
+
 extern crate libc;
 
 use libc::c_int;
