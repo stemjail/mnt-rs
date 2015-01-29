c4c4418a13a5ce13fca3e159ce6b82617006cf32
Mickaël Salaün <mickael.salaun@ssi.gouv.fr>
2015-01-29 18:11:51 +0100

    mnt: Rust update - replace crate attributes

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,11 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#![allow(staged_unstable)]
-#![allow(staged_experimental)]
+#![feature(collections)]
+#![feature(core)]
+#![feature(io)]
+#![feature(libc)]
+#![feature(path)]
 
 extern crate libc;
 
