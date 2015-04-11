3fffb1eba2352de0607c8f992b8ef1ec67b30642
Mickaël Salaün <mic@digikod.net>
2015-04-11 15:24:11 +0200

    mnt: Add get_mount

--- src/lib.rs
+++ src/lib.rs
@@ -114,6 +114,23 @@ pub fn get_submounts(root: &Path) -> Result<Vec<MountEntry>, ParseError> {
     Ok(ret)
 }
 
+/// Get the mount point `target`.
+pub fn get_mount(target: &Path) -> Result<Option<MountEntry>, ParseError> {
+    let mut ret = None;
+    for mount in try!(MountIter::new_from_proc()) {
+        match mount {
+            Ok(m) => {
+                if *target == m.file {
+                    // Get the last entry
+                    ret = Some(m);
+                }
+            },
+            Err(e) => return Err(e),
+        }
+    }
+    Ok(ret)
+}
+
 
 pub trait VecMountEntry {
     fn remove_overlaps(self, exclude_files: &Vec<&Path>) -> Self;
